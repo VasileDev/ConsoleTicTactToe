d2317e76ebaa5dbf2e8811609be35866d477d5ac
vas <vasile.vio200@gmail.com>
2024-09-06 15:38:49 +0300

    Finished the game!

--- main.cpp
+++ main.cpp
@@ -34,6 +34,7 @@ class Table {
             {1,2,2,2,2,2,2,2,1,2,2,2,2,2,2,2,1,2,2,2,2,2,2,2,1}
         };
         int remainingChoices[9] = {1, 2, 3, 4, 5, 6, 7, 8, 9}; 
+        int remainingSize = 9;
     public:
         void displayTable() {
             for (int i = 0; i < 13; ++i) {
@@ -45,9 +46,6 @@ class Table {
                     else if (table[i][j] == 6) std::cout << "0";
                     else std::cout << " ";
                 }
-
-                if (i == 12)
-                    continue;
                 std::cout << std::endl;
             }
         }
@@ -56,15 +54,147 @@ class Table {
             return remainingChoices;
         }
 
-        void setRemainingChoices(int arr[9]) {
-            for (int i = 0; i < 9; i++) {
-                remainingChoices[i] = arr[i];
-            }
+        // Getting the len of the remainingChoices array
+        int getSizeOfRemainingChoices() {
+            return remainingSize;
         }
 
         // Will remove a number from remainingChoices when a player makes a move
         void removeOption(int chosenNumber) {
+            // remainingSize = deleteElement(remainingChoices, remainingSize, chosenNumber);
+            for (int i = 0; i < remainingSize; i++) {
+                if (remainingChoices[i] == chosenNumber) {
+                    // Shift the remaining elements to the left
+                    for (int j = i; j < remainingSize - 1; j++) {
+                        remainingChoices[j] = remainingChoices[j + 1];
+                    }
+                    remainingSize--;  // Reduce the size after removing
+                    break;
+                }
+            }
+        }
 
+        void drawX(int nr) {
+            if (nr == 1) {
+                table[2][4] = 5;
+            } else if (nr == 2) {
+                table[2][12] = 5;
+            } else if (nr == 3) {
+                table[2][20] = 5;
+            } else if (nr == 4) {
+                table[6][4] = 5;
+            } else if (nr == 5) {
+                table[6][12] = 5;
+            } else if (nr == 6) {
+                table[6][20] = 5;
+            } else if (nr == 7) {
+                table[10][4] = 5;
+            } else if (nr == 8) {
+                table[10][12] = 5;
+            } else if (nr == 9) {
+                table[10][20] = 5;
+            }
+        }
+
+        void draw0(int nr) {
+            if (nr == 1) {
+                table[2][4] = 6;
+            } else if (nr == 2) {
+                table[2][12] = 6;
+            } else if (nr == 3) {
+                table[2][20] = 6;
+            } else if (nr == 4) {
+                table[6][4] = 6;
+            } else if (nr == 5) {
+                table[6][12] = 6;
+            } else if (nr == 6) {
+                table[6][20] = 6;
+            } else if (nr == 7) {
+                table[10][4] = 6;
+            } else if (nr == 8) {
+                table[10][12] = 6;
+            } else if (nr == 9) {
+                table[10][20] = 6;
+            }
+        }
+
+        bool checkAvailability(int theChoice) {
+            for (int i = 0; i < getSizeOfRemainingChoices(); i++) {
+                if (theChoice == remainingChoices[i]) {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        // Print the remaining choices
+        void printRemainingChoices() {
+            for (int i = 0; i < remainingSize; i++) {
+                std::cout << remainingChoices[i] << " ";
+            }
+            std::cout << std::endl;
+        }
+
+        int checkRows() {
+            if (table[2][4] == table[2][12] && table[2][12] == table[2][20]) {
+                return table[2][4];
+            }
+            if (table[6][4] == table[6][12] && table[6][12] == table[6][20]) {
+                return table[6][4];
+            }
+            if (table[10][4] == table[10][12] && table[10][12] == table[10][20]) {
+                return table[10][4];
+            }
+            return 55;
+        }
+
+        int checkColumns() {
+            if (table[2][4] == table[6][4] && table[6][4] == table[10][4]) {
+                return table[2][4];
+            }
+            if (table[2][12] == table[6][12] && table[6][12] == table[10][12]) {
+                return table[6][4];
+            }
+            if (table[2][20] == table[6][20] && table[6][20] == table[10][20]) {
+                return table[10][4];
+            }
+            return 56;
+        }
+
+        int checkDiagonals() {
+            if (table[2][4] == table[6][12] && table[6][12] == table[10][20]) {
+                return table[2][4];
+            }
+            if (table[2][20] == table[6][12] && table[6][12] == table[10][4]) {
+                return table[2][4];
+            }
+            return 57;
+        }
+
+        int checkWinner() {
+            if (checkRows() == 55) {
+
+            } else {
+                if (checkRows() == 5) {
+                    return 5;
+                } else return 6;
+            }
+
+            if (checkColumns() == 56) {
+
+            } else {
+                if (checkColumns() == 5) {
+                    return 5;
+                } else return 6;
+            }
+
+            if (checkDiagonals() == 57) {
+
+            } else {
+                if (checkDiagonals() == 5) {
+                    return 5;
+                } else return 6;
+            }
         }
 };
 
@@ -83,9 +213,9 @@ class Game {
             player1.setName(name1);
 
             std::string name2;
-            std::cout << "Choose a name for Player 2: " << "\n";
+            std::cout << "Choose a name for Player 2: ";
             getline(std::cin, name2);
-            player1.setName(name2);
+            player2.setName(name2);
         }
 
         void chooseStarter() {
@@ -114,36 +244,53 @@ class Game {
             player2.setTurn(!player2.getTurn());
         }
 
-        void makeMove(char mark) {
-            
+        void makeMove(char mark, int choice) {
+            if (mark == 'X') {
+                table.drawX(choice);
+            } else {
+                table.draw0(choice);
+            }
         }
 
         void playGame() {
             bool running = true;
             while (running) {
-                std::cout << "Choose a number: ";
-                int* remainingChoices = table.getremainingChoices();
-                for (int i = 0; i < 9; i++) {
-                    std::cout << *(remainingChoices + i) << " ";
-                }
+                table.printRemainingChoices();
+                std::cout << "Choose a number from above: ";
                 int choice;
                 // If the player inputs another things
                 while (true) {
                     std::cin >> choice;
-                    if (choice < 1 || choice > 9) {
-                        std::cout << "Please input a number from 1-10." << "\n";
-                    } else {
+                    
+                    if ((choice >= 1 && choice <= 9) && (table.checkAvailability(choice))) {
                         break;
+                    } else {
+                        std::cout << "Please input a number from the available choices." << "\n";
                     }
                 }
                 if (player1.getTurn() == true) {
-                    makeMove('X');
+                    makeMove('X', choice);
                     switchTurns();
                     table.removeOption(choice);
+                    table.displayTable();
                 } else {
-                    makeMove('0');
+                    makeMove('0', choice);
                     switchTurns();
                     table.removeOption(choice);
+                    table.displayTable();
+                    table.checkWinner();
+                }
+                if (table.checkWinner() == 5) {
+                    std::cout << player1.getName() << " won!" << "\n";
+                    running = false;
+                }
+                if (table.checkWinner() == 6) {
+                    std::cout << player2.getName() << " won!" << "\n";
+                    running = false;
+                }
+                if (table.getSizeOfRemainingChoices() == 0) {
+                    std::cout << "Nobody won this game!" << "\n";
+                    running = false;
                 }
             }
         }
